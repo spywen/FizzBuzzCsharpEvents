dfd6c35d6889ca5e48bc3d2e4b14eaebbb469e56
Laurent Babin <spywen@hotmail.fr>
2015-12-10 21:54:43 +0100

    Reduce event call size

--- FizzBuzzCsharpEvents/Counter.cs
+++ FizzBuzzCsharpEvents/Counter.cs
@@ -17,63 +17,26 @@ namespace FizzBuzzCsharpEvents
             Total++;
             if (Total >= Limit)
             {
-                OnEnd(new EventArgs());
+                if(End != null) End(this, EventArgs.Empty);
             }
             else if (Total % 3 == 0)
             {
-                OnDivisibleBy3(new EventArgs());
+                if (Fizz != null) Fizz(this, EventArgs.Empty);
             }
             else if (Total%5 == 0)
             {
-                OnDivisibleBy5(new EventArgs());
+                if (Buzz != null) Buzz(this, EventArgs.Empty);
             }
             else
             {
-                OnNotDivisibleBy3Or5(new IntegerEventArgs{Value = Total});
+                if (Other != null) Other(this, new IntegerEventArgs{Value = Total});
             }
         }
-
 
         public event EventHandler<EventArgs> End;
-        protected virtual void OnEnd(EventArgs e)
-        {
-            EventHandler<EventArgs> handler = End;
-            if (handler != null)
-            {
-                handler(this, e);
-            }
-        }
-
-
         public event EventHandler<EventArgs> Fizz;
-        protected virtual void OnDivisibleBy3(EventArgs e)
-        {
-            EventHandler<EventArgs> handler = Fizz;
-            if (handler != null)
-            {
-                handler(this, e);
-            }
-        }
-
         public event EventHandler<EventArgs> Buzz;
-        protected virtual void OnDivisibleBy5(EventArgs e)
-        {
-            EventHandler<EventArgs> handler = Buzz;
-            if (handler != null)
-            {
-                handler(this, e);
-            }
-        }
-
         public event EventHandler<IntegerEventArgs> Other;
-        protected virtual void OnNotDivisibleBy3Or5(IntegerEventArgs e)
-        {
-            EventHandler<IntegerEventArgs> handler = Other;
-            if (handler != null)
-            {
-                handler(this, e);
-            }
-        }
     }
 
     public class IntegerEventArgs : EventArgs
